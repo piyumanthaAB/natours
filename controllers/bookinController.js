d95739c692992e521071fb884c831078e4aa6cf4
Cherin Piyumantha <cherinpiyumantha@gmail.com>
2021-03-05 20:04:02 +0530

    corrected typo in checkout.session.completed

--- controllers/bookinController.js
+++ controllers/bookinController.js
@@ -73,7 +73,7 @@ exports.webhookCheckout = (req, res, next) => {
         return res.status(400).send(`Webhook error: ${err.message}`);
     }
 
-    if (event.type == 'checkout.session.complete') {
+    if (event.type == 'checkout.session.completed') {
         createBookingCheckout(event.data.object);
     }
 
